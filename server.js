de5664cf7d7a955f03089e6ec29a24869c99d59d
thomaspovinelli <tompov227@gmail.com>
2020-05-11 17:20:03 -0400

    added some server logging

--- server.js
+++ server.js
@@ -14,12 +14,19 @@ const limiter = rateLimit({
     max: 50
 })
 
+app.use((req, res, next) => {
+    let now = (new Date(Date.now()));
+    console.log(`Incoming ${req.method} request for "${req.url}" on ${now.toLocaleDateString()} at ${now.toLocaleTimeString()}`);
+    next();
+})
 app.use(limiter);
 app.use(express.static('public'));
 
 app.get('/get-bazaar-prices', (req, res) => {
+    console.log("Fetching from Hypixel");
     fetch(`https://api.hypixel.net/skyblock/bazaar?key=${process.env.KEY}`).then(r => r.json()).then(data => res.json(data));
 });
 
 
+console.log("Listening...")
 app.listen(process.env.PORT || 8000);
