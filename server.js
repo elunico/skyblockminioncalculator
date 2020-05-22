dae41e3d631cd150f3a5583ff9f1f5ff5edce1f3
Thomas Povinelli <tompov227@gmail.com>
2020-05-21 22:53:21 -0400

    Fixed a very silly null problem

--- server.js
+++ server.js
@@ -9,41 +9,41 @@ if (environment != 'production') {
 }
 
 
-const limiter = rateLimit({
-    windowMs: 1000 * 60,
-    max: 250
-});
+const limiter = rateLimit({ windowMs: 1000 * 60, max: 250 });
 
-const needFetchDifference = 1000 * 60 * 10 // 10 minutes 
+const needFetchDifference = 1000 * 60 * 10  // 10 minutes
 let lastFetch = 0;
 let cachedPrices = null;
 
 app.use((req, res, next) => {
     let now = (new Date(Date.now()));
-    console.log(`Incoming ${req.method} request for "${req.url}" on ${now.toLocaleDateString()} at ${now.toLocaleTimeString()}`);
+    console.log(`Incoming ${req.method} request for "${req.url}" on ${
+        now.toLocaleDateString()} at ${now.toLocaleTimeString()}`);
     next();
 })
 app.use(limiter);
 app.use(express.static('public'));
 
 app.get('/get-bazaar-prices', (req, res) => {
-
-    if (Date.now() - lastFetch > needFetchDifference) {
-        console.log("Fetching from Hypixel");
-        fetch(`https://api.hypixel.net/skyblock/bazaar?key=${process.env.KEY}`).then(r => r.json()).then(data => {
-            lastFetch = Date.now();
-            cachedPrices = data;
-            res.json(data);
-        }).catch(err => {
-            res.status(500);
-            res.json(err);
-        });
+    if ((!cachedPrices) || ((Date.now() - lastFetch) > needFetchDifference)) {
+        console.log('Fetching from Hypixel');
+        fetch(`https://api.hypixel.net/skyblock/bazaar?key=${process.env.KEY}`)
+            .then(r => r.json())
+            .then(data => {
+                lastFetch = Date.now();
+                cachedPrices = data;
+                res.json(data);
+            })
+            .catch(err => {
+                res.status(500);
+                res.json(err);
+            });
     } else {
-        console.log("Returning cached data");
+        console.log('Returning cached data');
         res.json(cachedPrices);
     }
 });
 
 
-console.log("Listening...")
+console.log('Listening...')
 app.listen(process.env.PORT || 8000);
